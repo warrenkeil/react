73432f4dee321c05e102859fe03053c100bbcfe1
Warren Keil <warren@deepforktechnology.com>
2020-11-20 09:30:51 -0600

    commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+var element = React.createElement('h1',{className: 'greeting'}, 'Hello, World!');
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
 
